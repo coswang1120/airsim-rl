d2e784699dbdf2dfa9078a42d0018fe68afa1403
ypkoo <ypkoo93@gmail.com>
2018-07-03 19:48:58 +0900

    green multirotor added

---
+++ airsim_rl/envs/green_multirotor_client.py
@@ -0,0 +1,31 @@
+
+class GreenMultirotorClient(object):
+
+	def __init__(self):
+		pass
+
+	def take_action(self, action):
+		"""
+		Args:
+			action: 
+
+		Returns:
+
+		"""
+		pass
+
+	def get_state(self):
+		"""
+		Returns:
+			observation: agent's observation of the current environment
+		"""
+		pass
+
+	def reset(self):
+		"""
+		Resets the state of the environment and returns an initial observation.
+		
+		Returns:
+			observation: the initial observation
+		"""
+		pass
