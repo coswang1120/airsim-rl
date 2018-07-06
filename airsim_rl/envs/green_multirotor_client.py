c3935480bd546378c208499e279490c63840de50
sennykim <sradiant@kaist.ac.kr>
2018-07-06 14:09:18 +0900

    basic methods

--- airsim_rl/envs/green_multirotor_client.py
+++ airsim_rl/envs/green_multirotor_client.py
@@ -7,18 +7,21 @@ VEL_X, VEL_Y, VEL_Z, ACC_X, ACC_Y, ACC_Z, ROLL, PITCH, YAW = range(NUM_SENSOR_ST
 class GreenMultirotorClient(MultirotorClient):
 
 	def __init__(self):
-		pass
+		self.img1 = None
+        self.img2 = None
+        MultirotorClient.__init__(self)
+        MultirotorClient.confirmConnection(self)
+        self.enableApiControl(True)
+        self.armDisarm(True)
+
+	def _take_off(self):
+        self.takeoffAsync(timeout_sec = 4).join()
+        self.moveToPositionAsync(0,0,-1.3,2).join()
+        self.hoverAsync().join()
 
 	def _take_action(self, action):
-		"""
-		Args:
-			action: (vel_x, vel_y, yaw_rate)
-
-		Returns:
-			collided: True if collision occured
-
-		"""
-		pass
+		self.moveByVelocityAsync((vel_x), (vel_y), 0, 0.1,drivetrain = airsim.DrivetrainType.MaxDegreeOfFreedom, yaw_mode=airsim.YawMode(True, yaw)).join()
+        self.hoverAsync().join()
 
 	def _get_state(self):
 		"""
@@ -29,10 +32,10 @@ class GreenMultirotorClient(MultirotorClient):
 		pass
 
 	def _reset(self):
-		"""
-		Resets the state of the environment and returns an initial observation.
-
-		Returns:
-			observation: the initial observation
-		"""
-		pass
+		self.armDisarm(False)
+        self.reset()
+        self.enableApiControl(False)
+        self.armDisarm(True)
+        self.enableApiControl(True)
+        self.moveToPositionAsync(0,0,0)
+        self.hoverAsync().join()
